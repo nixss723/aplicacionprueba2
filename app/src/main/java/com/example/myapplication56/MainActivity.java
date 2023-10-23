0f2809120e3bfd21c6cbad323a8812d6852ef314
matiiguzman5 <matiiguzman03@gmail.com>
2023-10-23 18:55:44 -0300

    Creamos la accion para el boton

--- app/src/main/java/com/example/myapplication56/MainActivity.java
+++ app/src/main/java/com/example/myapplication56/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication56;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bottonLogin= findViewById(R.id.button2);
+        TextView helloUser = findViewById(R.id.textimg2);
+
+
+        bottonLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                helloUser.setText(R.string.newuserHello);
+
+            }
+        });
     }
 }
