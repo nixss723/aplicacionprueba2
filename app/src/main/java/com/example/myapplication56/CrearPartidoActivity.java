9ff0d58576a7e2a1285811e43dddaa38235d4940
matiiguzman5 <matiiguzman03@gmail.com>
2023-11-27 02:17:05 -0300

    Mejoramos un poco el codigo

--- app/src/main/java/com/example/myapplication56/CrearPartidoActivity.java
+++ app/src/main/java/com/example/myapplication56/CrearPartidoActivity.java
@@ -18,26 +18,25 @@ public class CrearPartidoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_partido);
 
-        // Obtener referencias a los elementos de la interfaz de usuario
+
         DatePicker datePicker = findViewById(R.id.datePicker);
         Spinner spinnerTipoCancha = findViewById(R.id.spinnerTipoCancha);
         TimePicker timePicker = findViewById(R.id.timePicker);
         EditText editUbicacion = findViewById(R.id.editUbicacion);
         Button btnCrearPartido = findViewById(R.id.btnCrearPartido);
 
-        // Manejar clics en el botón "Crear Partido"
+
         btnCrearPartido.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Obtener los valores de los campos del formulario
+
                 String fechaPartido = datePicker.getDayOfMonth() + "/" + (datePicker.getMonth() + 1) + "/" + datePicker.getYear();
                 String tipoCancha = spinnerTipoCancha.getSelectedItem().toString();
                 String horaPartido = timePicker.getHour() + ":" + timePicker.getMinute();
                 String ubicacionPartido = editUbicacion.getText().toString();
 
-                // Aquí puedes realizar la lógica para guardar los datos del partido
-                // Por ahora, simplemente mostramos los valores en un Toast como ejemplo
-                mostrarMensaje("Fecha: " + fechaPartido + "\nTipo de Cancha: " + tipoCancha + "\nHora: " + horaPartido + "\nUbicación: " + ubicacionPartido);
+
+                mostrarMensaje("Partido creado con Exito! ");
             }
         });
     }
--- app/src/main/java/com/example/myapplication56/MainActivity.java
+++ app/src/main/java/com/example/myapplication56/MainActivity.java
@@ -17,21 +17,23 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        // Ejemplo: Obtener el nombre del usuario (reemplaza esto con tu lógica de autenticación)
-        String nombreUsuario = "Juan"; // Aquí debes obtener el nombre del usuario autenticado
+        // NO ESTA CREADA LA LOGICA DEL LOGIN
+        String nombreUsuario = "Juan";
 
-        // Mostrar el saludo en el TextView
+
         TextView textHolaUsuario = findViewById(R.id.textHolaUsuario);
         textHolaUsuario.setText("HOLA " + nombreUsuario);
 
-        // Manejar clics en los botones de opciones
+
         Button btnCrearPartido = findViewById(R.id.btnCrearPartido);
         Button btnVerPartidos = findViewById(R.id.btnVerPartidos);
+
+
 
         btnCrearPartido.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Lógica para la opción "CREAR PARTIDO"
+
                 Intent intent = new Intent(MainActivity.this, CrearPartidoActivity.class);
                 startActivity(intent);
             }
